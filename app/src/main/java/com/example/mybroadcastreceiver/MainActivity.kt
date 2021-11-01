6a6fff1809e24edf12749025dce4c77bfd7f6e8d
yayanrw <yayanraw@gmail.com>
2021-11-01 13:10:33 +0700

    add main act

--- app/src/main/java/com/example/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/mybroadcastreceiver/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.mybroadcastreceiver
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.mybroadcastreceiver.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val SMS_REQUEST_CODE = 101
+    }
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.btnPermission?.setOnClickListener(this)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == SMS_REQUEST_CODE) {
+            when {
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onClick(v: View) {
+        when {
+            v.id == R.id.btn_permission -> PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
